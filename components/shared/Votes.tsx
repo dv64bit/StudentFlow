99e24876cdfa285ab00842f2c91d03b9db997f9f
Darshan Verma <darshanverma2002@gmail.com>
2024-05-20 10:06:21 +0530

    added some toast

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -10,6 +10,7 @@ import { formatNumber } from "@/lib/utils";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import React, { useEffect } from "react";
+import { toast } from "../ui/use-toast";
 
 interface Props {
   type: string;
@@ -41,11 +42,18 @@ const Votes = ({
       questionId: JSON.parse(itemId),
       path: pathname,
     });
+    return toast({
+      title: `Question ${!hasSaved ? "Saved in" : "Removed from"} your collection`,
+      variant: !hasSaved ? "default" : "destructive",
+    });
   };
 
   const handleVote = async (action: string) => {
     if (!userId) {
-      return;
+      return toast({
+        title: "Please log in",
+        description: "You must be logged in to perform this action",
+      });
     }
     if (action === "upvote") {
       if (type === "Question") {
@@ -65,8 +73,10 @@ const Votes = ({
           path: pathname,
         });
       }
-      // todo: show a toast
-      return;
+      return toast({
+        title: `Upvote ${!hasupVoted ? "Successful" : "Removed"}`,
+        variant: !hasupVoted ? "default" : "destructive",
+      });
     }
 
     if (action === "downvote") {
@@ -87,8 +97,10 @@ const Votes = ({
           path: pathname,
         });
       }
-      // todo: show a toast
-      return;
+      return toast({
+        title: `Downvote ${!hasupVoted ? "Successful" : "Removed"}`,
+        variant: !hasupVoted ? "default" : "destructive",
+      });
     }
   };
 
