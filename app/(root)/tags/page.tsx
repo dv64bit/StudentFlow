c4e9323d0c9602a51afa098586800d20ff8ddb93
Darshan Verma <darshanverma2002@gmail.com>
2024-05-21 22:56:07 +0530

    bug fix-> Tag section-> capitalize all tag

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -41,7 +41,7 @@ const Page = async ({ searchParams }: SearchParamsProps) => {
             <Link
               href={`/tags/${tag._id}`}
               key={tag._id}
-              className="shadow-light100_darknone"
+              className="shadow-light100_darknone capitalize"
             >
               <article className="background-light900_dark200 light-border flex w-full flex-col rounded-2xl border px-8 py-10 sm:w-[260px]">
                 <div className="background-light800_dark400 w-fit rounded-sm px-5 py-1.5">
