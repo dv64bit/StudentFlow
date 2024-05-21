5ce5c580818cbaeb65d8d5e6da9d8be1c13eca17
Darshan Verma <darshanverma2002@gmail.com>
2024-05-21 23:14:09 +0530

    Some css fix in global search

--- components/shared/search/GlobalResult.tsx
+++ components/shared/search/GlobalResult.tsx
@@ -65,10 +65,8 @@ const GlobalResult = () => {
   };
 
   return (
-    <div className="absolute bg-light-800 top-full z-10 mt-3 w-full rounded-xl py-5 shadow-sm">
-      <p className="text-dark400_light900 paragraph-semibold px-5">
-        <GlobalFilters />
-      </p>
+    <div className="absolute top-full z-10 mt-3 w-full rounded-xl bg-light-800 py-5 shadow-sm dark:bg-dark-400">
+      <GlobalFilters />
       <div className="my-5 h-[1px] bg-light-700/50 dark:bg-dark-500/50"></div>
       <div className="space-y-5">
         <p className="text-dark400_light900 paragraph-semibold px-5">
